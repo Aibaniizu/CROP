79ea5b88734d3636775ca8b5582f2a46c10fd9f6
mari <marliini@hotmail.com>
2014-03-28 14:04:55 +0200

    Navbarin tiedosto jonka kanssa työskentelen

---
+++ navbar.php
@@ -0,0 +1,11 @@
+﻿<form method='get' action='profiili_listaus.php'>
+
+[ <a href=''> Etusivu </a> ]
+[ <a href='tapahtumat_listaa.php'> Tapahtumat </a> ]
+[ <a href=''> Yhteystiedot </a> ]
+[ <a href=''> Ohjeet </a> ]
+[ <a href=''> Ostoskori </a> ]
+[ <a href='profiilin_tiedot.php'> Oma profiili </a> ]
+
+<p>Haku: </p> <input type='text' size='10' name='hakuehto' value=''>
+</form>
