be1e06344efa39e55feb1ea306acd3193e3403bf
Aibaniizu <tiialle@windowslive.com>
2014-03-28 11:26:21 +0200

    Tällänen tiedoto, mihin kasaan ostoskoria

---
+++ ostoskori.php
@@ -0,0 +1,106 @@
+﻿<?php
+session_start();
+
+$vw_lkm = isset($_SESSION['vw_lkm']) ? $_SESSION['vw_lkm'] : 0;
+
+if(){
+$_SESSION['vw_lkm'] = $vw_lkm;
+}
+
+?>
+
+
+
+
+<?php
+// h3t2-autolaskuri-sessio.php
+
+// Pääohjelma
+session_start();
+
+$vw_lkm = isset($_SESSION['vw_lkm']) ? $_SESSION['vw_lkm'] : 0;
+$opel_lkm =isset($_SESSION['opel_lkm']) ? $_SESSION['opel_lkm'] : 0;
+$toyota_lkm = isset($_SESSION['toyota_lkm']) ? $_SESSION['toyota_lkm'] : 0;
+$painike = isset($_POST['painike']) ? $_POST['painike'] : '';
+
+laske_lkm($vw_lkm, $opel_lkm, $toyota_lkm, $painike);
+aseta_arvot($vw_lkm, $opel_lkm, $toyota_lkm);
+lomake();
+nayta_tulokset($vw_lkm, $opel_lkm, $toyota_lkm);
+
+
+// Alustetaan tai päivitetään autojen lukumääriä:
+// Viittaukset (&) välittyvät "takaisin" kutsuvaan ohjelmalohkooon
+function laske_lkm(&$vw_lkm, &$opel_lkm, &$toyota_lkm, $nappi)
+{
+   if (isset($nappi))
+   {    //         Lisätään kertymää tai nollataan:
+      if ($nappi == "VW")
+      {
+         $vw_lkm = $vw_lkm + 1;
+      }
+      elseif ($nappi == "Opel")
+      {
+         $opel_lkm = $opel_lkm + 1;
+      }
+      elseif ($nappi == "Toyota")
+      {
+         $toyota_lkm = $toyota_lkm + 1;
+      }
+      elseif ($nappi == "Nollaa") // Painettiin Nollaa-painiketta
+      {
+         $vw_lkm = 0;
+         $opel_lkm = 0;
+         $toyota_lkm = 0;
+      }
+   }
+}
+
+function nayta_tulokset($vw_lkm, $opel_lkm, $toyota_lkm)
+{
+   echo "<pre>\n";
+   echo "Volkswagenit ... : $vw_lkm kpl.\n";
+   echo "Opelit ......... : $opel_lkm kpl.\n";
+   echo "Toyotat ........ : $toyota_lkm kpl.\n</pre>";
+}
+
+// Syöttölomake autojen laskentaan.
+function lomake()
+{
+?>
+
+   <form method="POST" action="<?php echo "{$_SERVER['PHP_SELF']}"?>">
+
+   <!--
+    Huomaa useat samannimiset painikkeet: Arvona välittyy
+    _vain_ sen painikkeen arvo, jota painettiin!
+   -->
+
+   <input type="submit" value="VW" name="painike">
+   <input type="submit" value="Opel" name="painike">
+   <input type="submit" value="Toyota" name="painike">
+   <input type="submit" value="Nollaa" name="painike">
+   </form>
+<?php
+}
+
+function do_header()
+{
+?>
+
+<title>Autolaskuri</title>
+<h3 style=background-color:#fed;color:#000>Autolaskuri</h3>
+
+<?php
+}
+
+
+function aseta_arvot($vw_lkm, $opel_lkm, $toyota_lkm)
+{
+     $_SESSION['vw_lkm'] = $vw_lkm;
+     $_SESSION['opel_lkm'] = $opel_lkm ;
+     $_SESSION['toyota_lkm'] = $toyota_lkm;
+}
+
+
+?>
