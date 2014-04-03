ae5988b5d0ada1f5e2e0d5a0d3efdad3bee805e4
DevilSpy <G7753@student.jamk.fi>
2014-04-03 17:21:22 +0300

    no message

--- tapahtumat_listaa.php
+++ tapahtumat_listaa.php
@@ -15,14 +15,70 @@ $hakuehto = isset($_GET['hakuehto']) ? $_GET['hakuehto'] : '';
 $stmt = haeTapahtumat($db, $hakuehto);
 sqlResult2Html($stmt);
 
-// 
-function haeTapahtumat($db, $hakuehto) {
-   $sql = <<<SQLEND
-   SELECT tapahtumaID, nimi, ajankohta, jarjestaja, kuvaus, paikka, lipunhinta, lippukiintio, lippuostorajoitus, kuva, lisatiedot
-   FROM Tapahtuma WHERE tapahtumaID
-   LIKE :hakuehto
-SQLEND;
 
+// tapahtumien järjestys
+function haeTapahtumat($db, $hakuehto) {
+
+
+
+	
+	if(!isset($_GET['jarjestys'])) {
+	   $sql = <<<SQLEND
+	   SELECT tapahtumaID, nimi, ajankohta, jarjestaja, kuvaus, paikka, lipunhinta, lippukiintio, lippuostorajoitus, kuva, lisatiedot
+	   FROM Tapahtuma WHERE tapahtumaID
+	   LIKE :hakuehto
+SQLEND;
+	} else if(isset($_GET['jarjestys']) && $_GET['jarjestys'] == 'TapahtumaID') {
+		$sql = <<<SQLEND
+			 SELECT tapahtumaID, nimi, ajankohta, jarjestaja, kuvaus, paikka, lipunhinta, lippukiintio, lippuostorajoitus, kuva, lisatiedot
+			FROM Tapahtuma WHERE tapahtumaID
+			LIKE :hakuehto 
+			ORDER BY tapahtumaID
+SQLEND;
+	}else if(isset($_GET['jarjestys']) && $_GET['jarjestys'] == 'Tapahtuma') {
+		$sql = <<<SQLEND
+			 SELECT tapahtumaID, nimi, ajankohta, jarjestaja, kuvaus, paikka, lipunhinta, lippukiintio, lippuostorajoitus, kuva, lisatiedot
+			FROM Tapahtuma WHERE tapahtumaID
+			LIKE :hakuehto 
+			ORDER BY nimi
+SQLEND;
+	} else if(isset($_GET['jarjestys']) && $_GET['jarjestys'] == 'Ajankohta') {
+		$sql = <<<SQLEND
+			 SELECT tapahtumaID, nimi, ajankohta, jarjestaja, kuvaus, paikka, lipunhinta, lippukiintio, lippuostorajoitus, kuva, lisatiedot
+			FROM Tapahtuma WHERE tapahtumaID
+			LIKE :hakuehto 
+			ORDER BY ajankohta
+SQLEND;
+	} else if(isset($_GET['jarjestys']) && $_GET['jarjestys'] == 'Jarjestaja') {
+		$sql = <<<SQLEND
+			 SELECT tapahtumaID, nimi, ajankohta, jarjestaja, kuvaus, paikka, lipunhinta, lippukiintio, lippuostorajoitus, kuva, lisatiedot
+			FROM Tapahtuma WHERE tapahtumaID
+			LIKE :hakuehto 
+			ORDER BY jarjestaja
+SQLEND;
+	} else if(isset($_GET['jarjestys']) && $_GET['jarjestys'] == 'Kuvaus') {
+		$sql = <<<SQLEND
+			 SELECT tapahtumaID, nimi, ajankohta, jarjestaja, kuvaus, paikka, lipunhinta, lippukiintio, lippuostorajoitus, kuva, lisatiedot
+			FROM Tapahtuma WHERE tapahtumaID
+			LIKE :hakuehto 
+			ORDER BY kuvaus
+SQLEND;
+	} else if(isset($_GET['jarjestys']) && $_GET['jarjestys'] == 'Paikka') {
+		$sql = <<<SQLEND
+			 SELECT tapahtumaID, nimi, ajankohta, jarjestaja, kuvaus, paikka, lipunhinta, lippukiintio, lippuostorajoitus, kuva, lisatiedot
+			FROM Tapahtuma WHERE tapahtumaID
+			LIKE :hakuehto 
+			ORDER BY paikka
+SQLEND;
+	} else if(isset($_GET['jarjestys']) && $_GET['jarjestys'] == 'Hinta') {
+		$sql = <<<SQLEND
+			 SELECT tapahtumaID, nimi, ajankohta, jarjestaja, kuvaus, paikka, lipunhinta, lippukiintio, lippuostorajoitus, kuva, lisatiedot
+			FROM Tapahtuma WHERE tapahtumaID
+			LIKE :hakuehto 
+			ORDER BY lipunhinta
+SQLEND;
+	}
+	
    $stmt = $db->prepare("$sql");
    $stmt->bindValue(':hakuehto', "%$hakuehto%", PDO::PARAM_STR);
    $stmt->execute();
@@ -37,14 +93,21 @@ $col_count  = $stmt->columnCount();
 
 echo "Hakutulokset:" . $row_count. " riviä:<hr>\n";
 echo "<table border='0'>\n";    
+// yläpalkki
 $output = <<<OUTPUTEND
 <tr bgcolor='#ffeedd'>
-<td>TapahtumaID</td><td>Tapahtuma</td><td>Ajankohta</td>
-<td>Järjestäjä</td><td>Kuvaus</td><td>Paikka</td><td>Hinta</td>
+<td><a href='tapahtumat_listaa.php?jarjestys=TapahtumaID'>TapahtumaID</a></td>
+<td><a href='tapahtumat_listaa.php?jarjestys=Tapahtuma'>Tapahtuma</a></td>
+<td><a href='tapahtumat_listaa.php?jarjestys=Ajankohta'>Ajankohta</a></td>
+<td><a href='tapahtumat_listaa.php?jarjestys=Jarjestaja'>Järjestäjä</a></td>
+<td><a href='tapahtumat_listaa.php?jarjestys=Kuvaus'>Kuvaus</a></td>
+<td><a href='tapahtumat_listaa.php?jarjestys=Paikka'>Paikka</a></td>
+<td><a href='tapahtumat_listaa.php?jarjestys=Hinta'>Hinta</a></td>
 </tr>
 OUTPUTEND;
 echo $output;
 
+// tapahtumat
 while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
     $muokkaus_linkki = "<a href='tapahtumat_muokkauslomake.php?id={$row['tapahtumaID']}'>{$row['tapahtumaID']}</a>";
 	$tapahtuma_linkki = "<a href='tapahtumat_tapahtumasivu.php?id={$row['tapahtumaID']}'>{$row['nimi']}</a>";
